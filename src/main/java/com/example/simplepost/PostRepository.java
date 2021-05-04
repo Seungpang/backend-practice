03dfccacb581159470d7f8c13d47b55f3aa87f54
Seungpang <obey1342@gmail.com>
2021-05-04 13:10:02 +0900

    feat: 글 내용으로 검색 추가

--- src/main/java/com/example/simplepost/PostRepository.java
+++ src/main/java/com/example/simplepost/PostRepository.java
@@ -1,8 +1,11 @@
 package com.example.simplepost;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findByContentContains(String content);
 }
